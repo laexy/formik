1ca8640b08b40d13367acecce0987d123f7659ac
laexy <alex31wk@gmail.com>
2022-10-06 13:30:47 +1100

    Initial upload

---
+++ src/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Signup() {
+  return (
+    <form>
+      <div className='input-container'>
+        <input type='text' id='firstName' name='firstName' placeholder='First Name'/>
+        
+      </div>
+    </form>
+  );
+}
