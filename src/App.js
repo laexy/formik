19a5c14f2969b83e1d049915e043d24c42ceb654
laexy <alex31wk@gmail.com>
2022-10-06 13:38:47 +1100

    Standard way of dealing with react forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Signup from './Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Signup />
     </div>
   );
 }
--- src/Signup.js
+++ src/Signup.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState('');
+
+  console.log(firstName);
+
   return (
     <form>
       <div className='input-container'>
-        <input type='text' id='firstName' name='firstName' placeholder='First Name'/>
-        
+        <input
+          type='text'
+          id='firstName'
+          name='firstName'
+          placeholder='First Name'
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
       </div>
     </form>
   );
